39c9b107128a5d6cbd162a1a60dec2525bb5340b
ClauCll <63939747+ClauCll@users.noreply.github.com>
2020-04-23 20:55:04 -0500

    Update missing_data.py

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/missing_data.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/missing_data.py
@@ -21,5 +21,6 @@ y = dataset.iloc[:, 3].values
 # Tratamiento de los NAs
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose=0) 
+# imputer = imputer(missing_values ="NaN", strategy = "mean", axis=0
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:,1:3])
